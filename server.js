c464eec24095734c381b2cfd6a4cc5484950b317
Mert Yurt <mert-yurt@hotmail.com>
2020-09-24 20:56:06 +0300

    adding port

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.post("/register", (req, res)=>{ register.handleRegister(req, res, db ,bcrypt
 app.put("/image", (req, res) =>{image.handleImage(req, res, db)})
 app.post("/imageurl", (req, res) =>{image.handleApiCall(req, res)})
 
-app.listen(3000, ()=>{
-    console.log("apps running on port 3000")
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`apps running on port ${process.env.PORT}`)
 })
 
 /*
